588168bf2bbdb4b4314f0561cd837059889b8a20
Andrew Toth <andrewstoth@gmail.com>
2021-02-25 10:07:39 -0500

    Replace sha.js with create-hash

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
 'use strict';
-const sha = require('sha.js');
+const createHash = require('create-hash');
 class TaggedHash {
     constructor(tag) {
-        const prefixHalf = sha('sha256').update(tag).digest();
+        const prefixHalf = createHash('sha256').update(tag).digest();
         this.prefix = new Uint8Array([...prefixHalf, ...prefixHalf]);
     }
     hash(data) {
-        return sha('sha256').update(this.prefix).update(data).digest();
+        return createHash('sha256').update(this.prefix).update(data).digest();
     }
 }
 module.exports = TaggedHash;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 'use strict';
-const sha = require('sha.js');
+const createHash = require('create-hash');
 
 class TaggedHash {
   private prefix: Uint8Array;
 
   constructor(tag: string) {
-    const prefixHalf = sha('sha256').update(tag).digest();
+    const prefixHalf = createHash('sha256').update(tag).digest();
     this.prefix = new Uint8Array([...prefixHalf, ...prefixHalf]);
   }
 
   hash(data: Uint8Array): Uint8Array {
-    return sha('sha256').update(this.prefix).update(data).digest();
+    return createHash('sha256').update(this.prefix).update(data).digest();
   }
 }
 
